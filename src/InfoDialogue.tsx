f7bec8aeac4d757547eff8d27df86936bfb54c12
starwort <tcphone93@gmail.com>
2025-02-11 15:19:39 +0000

    Patch out info dialogue and trigger rebuild

--- src/InfoDialogue.tsx
+++ src/InfoDialogue.tsx
@@ -9,7 +9,7 @@ export function InfoDialogue(props: InfoDialogueProps) {
     return <Dialog {...props}>
         <DialogTitle>About</DialogTitle>
         <DialogContent>
-            <Typography>
+            {/* <Typography>
                 Tangleword is a word puzzle game, where several crossword-style clues
                 each point to a vertical column. The answer to each clue is a three-letter
                 word, made up of the three letters that the clue points to.
@@ -24,12 +24,12 @@ export function InfoDialogue(props: InfoDialogueProps) {
                 For convenience, an additional view is provided that shows the
                 letters in a grid. This may be easier to read for some people.
             </Typography>
-            <br />
+            <br /> */}
             <Typography>
-                This implementation of Tangleword, including the name 'Tangleword',
-                was created by Starwort, based on the game described in <Link
-                    href="https://www.theguardian.com/science/2024/mar/03/can-you-solve-it-the-word-game-at-the-cutting-edge-of-computer-science"
-                >Alex Bellos' Guardian article</Link>.
+                This implementation of Gerrymandering was created by Starwort,
+                based on the puzzle as described by <Link
+                    href="https://www.youtube.com/@v.deckard"
+                >Deckard</Link> in their two <Link href="https://www.youtube.com/watch?v=WQQrFOgRjDg">YouTube</Link> <Link href="https://www.youtube.com/watch?v=SvdJWcijn4M">Videos</Link>.
             </Typography>
             <Box sx={{display: "flex", justifyContent: "center", gap: 2, paddingTop: 2}}>
                 <Button href="https://ko-fi.com/starwort" startIcon={<Kofi />} variant="contained">
